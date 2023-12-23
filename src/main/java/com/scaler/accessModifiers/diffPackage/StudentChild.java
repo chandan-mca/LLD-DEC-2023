52825685bb4fcc2cfa3390de317707be59568262
Chandan Agrawal <chandan.acuver@gmail.com>
2023-12-23 15:40:27 +0530

    Access Modifiers

--- src/main/java/com/scaler/accessModifiers/diffPackage/StudentChild.java
+++ src/main/java/com/scaler/accessModifiers/diffPackage/StudentChild.java
@@ -3,12 +3,23 @@ package com.scaler.accessModifiers.diffPackage;
 import com.scaler.accessModifiers.myPackage.Student;
 
 public class StudentChild extends Student {
-    public static void main(String[] args) {
+    void m1() {
+        //id = 101;              // private not allowed
+        //name = "Chandan";      // default not allowed
+        gender = "Male";         // protected allowed
+        age = 21;                // public allowed
+    }
 
-        StudentChild student1 = new StudentChild();
-        //student1.id = 101;              // private not allowed
-        //student1.name = "Chandan";      // default not allowed
-        student1.gender = "Male";         // protected not allowed
-        student1.age = 21;                // public allowed
+    void m2() {
+
+        Student student = new Student();
+        //student.id = 101;              // private not allowed
+        //student.name = "Chandan";      // default not allowed
+        //student.gender = "Male";       // protected allowed
+        student.age = 21;                // public allowed
+
+        // Only public are going to be visible
+        // because if child tries to access attributes of a parent
+        // via an object of it, they are considered a third party code
     }
 }
--- src/main/java/com/scaler/accessModifiers/myPackage/Student.java
+++ src/main/java/com/scaler/accessModifiers/myPackage/Student.java
@@ -7,4 +7,11 @@ public class Student {
     protected String gender;
     public int age;
     public String batch;
+
+    private void pauseCourse(int duration) {
+        id = 123;
+        name = "ABC";
+        batch = null;
+        System.out.println("Paused the course");
+    }
 }
